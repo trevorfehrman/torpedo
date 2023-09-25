b50c153d64d677f84f7d51340857ac173b352c92
Trevor Fehrman <trevorfehrman@gmail.com>
2023-09-24 18:34:53 -0700

    changing date schema to bigint

--- db/schema.ts
+++ db/schema.ts
@@ -1,10 +1,10 @@
-import { mysqlTable, varchar, timestamp } from 'drizzle-orm/mysql-core';
+import { mysqlTable, varchar, timestamp, bigint } from 'drizzle-orm/mysql-core';
 
 export const users = mysqlTable('user', {
   id: varchar('id', { length: 64 }).primaryKey().notNull().unique(),
   firstName: varchar('first_name', { length: 30 }).notNull(),
   lastName: varchar('last_name', { length: 30 }).notNull(),
-  createdAt: timestamp('created_at').defaultNow(),
+  createdAt: bigint('created_at', { mode: 'number' }),
   emailAddress: varchar('email_address', { length: 100 }).notNull(),
   imageUrl: varchar('image_url', { length: 100 }).notNull(),
   username: varchar('username', { length: 60 }),
